04c36c2ee7c3d102e390022b386e968f8cf907d1
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-04 16:49:48 -0300

    Declaração de atributos private dos tables em sql.

---
+++ src/main/java/domain/event/Event.java
@@ -0,0 +1,24 @@
+package domain.event;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Event {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String detail;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false, name = "maximum_attendees")
+    private Integer maximumAttendees;
+}
