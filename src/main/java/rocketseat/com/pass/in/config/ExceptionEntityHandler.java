27e67451ddaea0451fb027f8ae7a902ec0cdac73
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-06 21:32:42 -0300

    Criação de Tratamento de exception.

--- src/main/java/rocketseat/com/pass/in/config/ExceptionEntityHandler.java
+++ src/main/java/rocketseat/com/pass/in/config/ExceptionEntityHandler.java
@@ -1,10 +1,16 @@
 package rocketseat.com.pass.in.config;
 
 import jdk.jshell.EvalException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import rocketseat.com.pass.in.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import rocketseat.com.pass.in.domain.attendee.exceptions.AttendeeNotFoundException;
+import rocketseat.com.pass.in.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import rocketseat.com.pass.in.domain.event.exceptions.EventFullException;
 import rocketseat.com.pass.in.domain.event.exceptions.EventNotFoundException;
+import rocketseat.com.pass.in.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
@@ -13,5 +19,29 @@ public class ExceptionEntityHandler {
     public ResponseEntity handlerEventNotFound (EventNotFoundException exception) {
 
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handlerEventFullException (EventFullException exception) {
+
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handlerAttendeeNotFoundException (AttendeeNotFoundException exception) {
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistException.class)
+    public ResponseEntity handlerAttendeeAlreadyExistException (AttendeeAlreadyExistException exception) {
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handlerCheckInAlreadyExistsException (CheckInAlreadyExistsException exception) {
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 }
---
+++ src/main/java/rocketseat/com/pass/in/dto/general/ErrorResponseDTO.java
@@ -0,0 +1,4 @@
+package rocketseat.com.pass.in.dto.general;
+
+public record ErrorResponseDTO(String message) {
+}
